dd9a251f1edd74ad43a1b3164857554e4fa3568e
victorpatilea <44482098+victorpatilea@users.noreply.github.com>
2018-10-26 02:41:53 +0300

    Add files via upload

---
+++ Numbers - Find PI to the Nth Digit/Karan.Numbers/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Karan.Numbers
+{
+    public static class Utilities
+    {
+        public static string GetPiToTheNthDigit(int n)
+        {
+            if(n < 1 || n > 14)
+            {
+                throw new ArgumentException("Must be between 1 and 14", nameof(n));
+            }
+
+            // PI can be calculated independently as (4 * Math.Atan(1d/5) - Math.Atan(1d/239)) * 4
+            // TODO: find way to calculate and store more than the digit limit for double
+            return Math.PI.ToString().Substring(0, n + 2);
+        }
+    }
+}
---
+++ Numbers - Find PI to the Nth Digit/Karan.Tests/NumbersTests.cs
@@ -0,0 +1,15 @@
+﻿using Karan.Numbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Karan.Tests
+{
+    [TestClass]
+    public class NumbersTests
+    {
+        [TestMethod]
+        public void PiToTheNthDigit()
+        {
+            Assert.IsTrue(Utilities.GetPiToTheNthDigit(10) == "3.1415926535");
+        }
+    }
+}
